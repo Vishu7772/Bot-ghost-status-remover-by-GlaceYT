dee1c21f56545e7b5e434ee1e2476b1ec30cae6e
Atrone <162317231+Vishu7772@users.noreply.github.com>
2024-09-29 12:22:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -37,8 +37,8 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🎧 Listening to Pheonix", "🎮 Playing Pheonix"];
-const statusTypes = [ 'dnd', 'idle'];
+const statusMessages = ["🎧 Listening to Pheonix"];
+const statusTypes = [ 'dnd'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
 
